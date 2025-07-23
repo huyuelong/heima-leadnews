12b7b531f4b153d92936cd92f1c4e6d1d6385e39
huyuelong <949910047@qq.com>
2025-07-23 18:04:36 +0800

    feat(article): 实现文章审核功能
    - 新增文章审核相关接口和实现类
    - 添加文章内容和图片的敏感词过滤
    - 集成阿里云文本和图片审核服务
    - 实现文章自动审核逻辑
    - 优化文章保存流程，支持新增和修改- 异步处理文章审核任务

---
+++ heima-leadnews-feign-api/src/main/java/com/heima/apis/article/fallback/IArticleClientFallback.java
@@ -0,0 +1,13 @@
+package com.heima.apis.article.fallback;
+
+import com.heima.apis.article.IArticleClient;
+import com.heima.model.article.dtos.ArticleDto;
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.common.enums.AppHttpCodeEnum;
+
+public class IArticleClientFallback implements IArticleClient {
+    @Override
+    public ResponseResult saveArticle(ArticleDto dto) {
+        return ResponseResult.errorResult(AppHttpCodeEnum.SERVER_ERROR,"保存数据失败");
+    }
+}
--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmNewsServiceImpl.java
@@ -22,6 +22,7 @@ import com.heima.utils.thread.WmThreadLocalUtil;
 import com.heima.wemedia.mapper.WmMaterialMapper;
 import com.heima.wemedia.mapper.WmNewsMapper;
 import com.heima.wemedia.mapper.WmNewsMaterialMapper;
+import com.heima.wemedia.service.WmNewsAutoScanService;
 import com.heima.wemedia.service.WmNewsService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -93,6 +94,9 @@ public class WmNewsServiceImpl extends ServiceImpl<WmNewsMapper, WmNews> impleme
         return responseResult;
     }
 
+    @Autowired
+    private WmNewsAutoScanService wmNewsAutoScanService;
+
     /**
      * 发布修改文章或保存为草稿
      *
@@ -135,6 +139,9 @@ public class WmNewsServiceImpl extends ServiceImpl<WmNewsMapper, WmNews> impleme
 
         // 4.不是草稿，保存文章封面图片和素材的关系，如果当前布局是自动，需要匹配封面图片
         saveRelativeInfoForCover(dto, wmNews, materials);
+
+        // 5.审核文章
+        wmNewsAutoScanService.autoScanWmNews(wmNews.getId());
 
         return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
     }
